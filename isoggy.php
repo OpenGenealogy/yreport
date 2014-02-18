cdbb061a979aad9ed0a4aca4be29776b2aa73fbb
Rob Hoare <bitbucket@robhoare.com>
2014-02-17 23:28:43 -0700

    formatting changes

--- isoggy.php
+++ isoggy.php
@@ -1,5 +1,11 @@
 <?php
-// read in index.txt (created by isoggy)
+// read in index.txt that was created by https://github.com/OpenGenealogy/isoggy
+// and match with Y-chromosone data from 23andme
+// Copyright 2014 Rob Hoare.  License: MIT.  
+// https://github.com/OpenGenealogy/yreport
+//
+// this is just proof of concept code, I will totally restructure!
+// ==================================
 $indexfile = file('index.txt'); // read index (make into arg)
 $row = 0;
 foreach ($indexfile as $index) {
@@ -21,8 +27,8 @@ foreach ($indexfile as $index) {
 	$row++;
 };
 //print_r($isogg);
-print count($isogg);
-print "\n\n";
+//print count($isogg);
+//print "\n\n";
 
 
 // ok, I've loaded the isogg data.  Now get my y dna:
@@ -40,16 +46,36 @@ if (($handle = fopen("y.txt", "r")) !== FALSE) {
 // now read through y-dna and lookup in y-tree index file
 
 foreach ($y as $ydata) {
+	if ($ydata[1] == 'Y') {
 	$pos = $ydata[2];
-	$val = @$isogg["$pos"]['ins'];
-	//echo "$ydata[2] $ydata[3] $val\n";
-	//print_r(@$isogg["$pos"]);
-	if ($ydata[3] == $val) {
-		$result[] = $isogg["$pos"]['sc']."\t".$pos."  \t"."$ydata[3]\t".$isogg["$pos"]['snp']."\t"."\n";	
-	};
+	$yval = $ydata[3]; 
+} else { // another data format, missing the second column
+	$pos = $ydata[1];
+	$yval = $ydata[2]; 
+};	
+    $del = @$isogg["$pos"]['del'];	
+	$val = @$isogg["$pos"]['ins'];		
+	if (@$isogg["$pos"]['sc']) {
+			$sc = $isogg["$pos"]['sc'];
+			$snp = $isogg["$pos"]['snp'];
+	} else {
+		$sc = '';
+		$snp = '';
+	};		
+	if ($yval == $val) {
+		$true = 'YES';
+
+	} else {
+		$true = 'no';
+	};	
+	$len = 100+strlen($sc);
+	$result[] = "$true$sc$pos|".$ydata[0]."\t".$pos."\t"."$yval\t$del->$val\t$true\t".$sc."\t".$snp."\t"."\n";	
+	
 };
 sort($result);
 //print_r($result);	
+echo "rsid\tposition\tgeno\tmutation\ttrue\thaplogroup\tsnp\n";
 foreach ($result as $r) {
-	echo $r;
+	$e = explode('|',$r,2);
+	echo $e[1];
 };	
